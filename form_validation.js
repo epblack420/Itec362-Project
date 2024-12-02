ad36f733c28e494fd4ce0da1bb8a6b352bd9e326
epblack420 <blackep55@gmail.com>
2024-12-02 14:10:44 -0500

    Update form_validation.js

--- form_validation.js
+++ form_validation.js
@@ -7,12 +7,12 @@
     const errors = [];
     // Validation for username (minimum length of 3 characters)
     if (nameInput.value.length < 3) {
-    errors.push("Username must be at least 3 characters long.");
+    errors.push("Your name must be at least 3 characters long.");
     }
     // Validation for email (must be a valid email format)
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(emailInput.value)) {
-    errors.push("Please enter a valid email address.");
+    errors.push("Please enter a valid email address");
     }
     // If there are errors, prevent form submission and display them
     if (errors.length > 0) {
